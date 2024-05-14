50cd408ec7b126a1ff9e4aa04aa5759e8d995c30
Daniel Bannert <prisis@users.noreply.github.com>
2024-05-15 00:14:32 +0200

    fix: updated core dep of @semrel-extra/topo, execa, semver and dev deps (#18)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,30 @@
 /// <reference types="vitest" />
+import { defineConfig, configDefaults, coverageConfigDefaults } from "vitest/config";
 
-import { defineConfig } from "vitest/config";
+const VITEST_SEQUENCE_SEED = Date.now();
+
+console.log("VITEST_SEQUENCE_SEED", VITEST_SEQUENCE_SEED);
 
 // https://vitejs.dev/config/
 export default defineConfig({
     test: {
+        ...configDefaults,
+        coverage: {
+            ...coverageConfigDefaults,
+            provider: "v8",
+            reporter: ["clover", "cobertura", "lcov", "text"],
+            include: ["src"],
+            exclude: ["__fixtures__/**", "__bench__/**", "scripts/**"],
+        },
         environment: "node",
-        dir: "./__tests__",
         testTimeout: 10000,
+        reporters: process.env.CI_PREFLIGHT ? ["basic", "github-actions"] : ["basic"],
+        sequence: {
+            seed: VITEST_SEQUENCE_SEED,
+        },
+        typecheck: {
+            enabled: false,
+        },
+        exclude: [...configDefaults.exclude, "__fixtures__/**"],
     },
 });
