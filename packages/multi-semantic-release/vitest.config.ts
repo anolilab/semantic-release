f7e21cb4527d604b7110aca485b1232bce0e95f8
prisis <d.bannert@anolilab.de>
2024-05-21 22:39:56 +0200

    chore: updated files from old main

--- packages/multi-semantic-release/vitest.config.ts
+++ packages/multi-semantic-release/vitest.config.ts
@@ -1,30 +1,5 @@
-/// <reference types="vitest" />
-import { defineConfig, configDefaults, coverageConfigDefaults } from "vitest/config";
+import { getVitestConfig } from "../../tools/get-vitest-config";
 
-const VITEST_SEQUENCE_SEED = Date.now();
+const config = getVitestConfig();
 
-console.log("VITEST_SEQUENCE_SEED", VITEST_SEQUENCE_SEED);
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    test: {
-        ...configDefaults,
-        coverage: {
-            ...coverageConfigDefaults,
-            provider: "v8",
-            reporter: ["clover", "cobertura", "lcov", "text"],
-            include: ["src"],
-            exclude: ["__fixtures__/**", "__bench__/**", "scripts/**"],
-        },
-        environment: "node",
-        testTimeout: 10000,
-        reporters: process.env.CI_PREFLIGHT ? ["basic", "github-actions"] : ["basic"],
-        sequence: {
-            seed: VITEST_SEQUENCE_SEED,
-        },
-        typecheck: {
-            enabled: false,
-        },
-        exclude: [...configDefaults.exclude, "__fixtures__/**"],
-    },
-});
+export default config;
