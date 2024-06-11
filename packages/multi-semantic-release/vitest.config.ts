f2e134f8cd8c106be204993865e4a129fac07a77
prisis <d.bannert@anolilab.de>
2024-06-11 20:05:17 +0200

    ci: display semantic-release version on test

--- packages/multi-semantic-release/vitest.config.ts
+++ packages/multi-semantic-release/vitest.config.ts
@@ -1,4 +1,11 @@
+import { readFile } from "fs/promises";
+
 import { getVitestConfig } from "../../tools/get-vitest-config";
+import { URL } from "url";
+
+const pkg = JSON.parse(await readFile(new URL("./node_modules/semantic-release/package.json", import.meta.url), { encoding: "utf-8" }));
+
+console.log("\nTesting with semantic-release version", pkg.version, "\n");
 
 const config = getVitestConfig();
 
